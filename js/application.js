5c922e980f4fb867deb3f88f18d3f1f10c48ce36
Tyler '-z-' Mulligan <z@interwebninja.com>
2013-02-20 23:12:25 -0500

    Removed the old useless junk and replaced with twitter bootstrap. Quick brain dump to get started but nothing fancy yet.

---
+++ js/application.js
@@ -0,0 +1,22 @@
+!function ($) {
+
+  $(function(){
+
+    var $window = $(window)
+
+    // Disable certain links in docs
+    $('section [href^=#]').click(function (e) {
+      e.preventDefault()
+    })
+
+    // side bar
+    setTimeout(function () {
+      $('.bs-docs-sidenav').affix({
+        offset: {
+          top: function () { return $window.width() <= 980 ? 290 : 210 }
+        , bottom: 270
+        }
+      })
+    }, 100)
+  })
+}(window.jQuery)
